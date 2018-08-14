79fb08b88a120abc3e286677fa9bd89115897699
Tim Fischer <emailtimfischer@gmail.com>
2018-08-13 19:01:11 -0600

    Round stat data to nearest hundreth

--- src/helper.js
+++ src/helper.js
@@ -5,15 +5,27 @@ export default class DistrictRepository {
 
   removeDuplicates = data => {
     return data.reduce((refinedData, stat) => {
-      if (!refinedData[stat.Location]) {
-        refinedData[stat.Location] = {};
+      let name = stat.Location.toUpperCase();
+      if (!refinedData[name]) {
+        refinedData[name] = {
+          location: name,
+          stats: {}
+        };
       }
-      refinedData[stat.Location][stat.TimeFrame] = stat.Data;
+      refinedData[name].stats[stat.TimeFrame] =
+        Math.round(stat.Data * 1000) / 1000 || 0;
       return refinedData;
     }, {});
   };
 
   findByName = search => {
-    // if (!search) return;
+    if (!search) return;
+    let upCase = search.toUpperCase();
+
+    if (this.stats[upCase]) {
+      return this.stats[upCase];
+    } else {
+      return undefined;
+    }
   };
 }
