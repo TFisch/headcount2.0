14078a7b28762f303ed6646b022026faa473fb8b
Tim Fischer <emailtimfischer@gmail.com>
2018-08-14 12:36:05 -0600

    Import DistrictList to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import DistrictList from './Components/DistrictList';
 
 class App extends Component {
   render() {
-    return <div>{/* <DistrictList /> */}</div>;
+    return (
+      <div>
+        <DistrictList />
+      </div>
+    );
   }
 }
 
