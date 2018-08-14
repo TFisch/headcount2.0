d6b534d7a505bf056d57d053dba26b5ba263105c
Tim Fischer <emailtimfischer@gmail.com>
2018-08-14 12:51:11 -0600

    Import DistrictCard component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import DistrictList from './Components/DistrictList';
+import DistrictCard from './Components/DistrictCard';
 
 class App extends Component {
   render() {
