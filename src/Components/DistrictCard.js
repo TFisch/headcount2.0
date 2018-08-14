60c17c22d7e8e180a5dc6dc449949037e120a4d4
Tim Fischer <emailtimfischer@gmail.com>
2018-08-14 15:21:24 -0600

    Adjust card year data margin

--- src/Components/DistrictCard.js
+++ src/Components/DistrictCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "../styling/DistrictCard.css"
 
 const DistrictCard = ({ location, stats }) => {
-  return <div>
+  return <div className="district-card">
     <h3>{location}</h3>
-    {Object.keys(stats).map(stat => <p>{stat} : {stats[stat]}</p>)}
+    {Object.keys(stats).map(stat => <p className="yearStats">{stat} : {stats[stat]}</p>)}
 
   </div>
 
