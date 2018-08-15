c766e16845f98abe49f249c197769aa0cd569895
Tim Fischer <emailtimfischer@gmail.com>
2018-08-14 22:12:24 -0600

    Add padding to yearStats

--- src/Components/DistrictCard.js
+++ src/Components/DistrictCard.js
@@ -4,8 +4,9 @@ import "../styling/DistrictCard.css"
 const DistrictCard = ({ location, stats }) => {
   return <div className="district-card">
     <h3 className="location-header">{location}</h3>
-    {Object.keys(stats).map(stat => <p className={stats[stat] < 0.5 ? 'lowStats' : 'yearStats'}>{stat} : {stats[stat]}</p>)}
-
+    <div className="stats-wrap">
+      {Object.keys(stats).map(stat => <p className={stats[stat] < 0.5 ? 'lowStats' : 'yearStats'}>{stat} : {stats[stat]}</p>)}
+    </div>
   </div>
 
 }
