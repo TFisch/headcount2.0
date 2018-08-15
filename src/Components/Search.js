9aab4255d81672cab482a03d21cf43b94cec8088
Tim Fischer <emailtimfischer@gmail.com>
2018-08-14 19:22:51 -0600

    Update set to searchInput

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import "../styling/Search.css"
+import kinderData from '../data/kindergartners_in_full_day_program';
+import DistrictRepository from '../helper';
+const districts = new DistrictRepository(kinderData);
 
 
 export default class Search extends Component {
   constructor() {
     super()
     this.state = {
-
+      searchInput: ""
     }
+  }
+
+  handleSubmit = (e) => {
+    console.log('hi')
+  }
+
+
+  handleChange = (e) => {
+    const searchInput = e.target.value;
+    this.setState({ searchInput })
   }
 
   render() {
     return (
       <div className="search-wrap">
-        <input className="search-input" />
+        <input className="search-input" value={this.state.searchInput} onChange={this.handleChange} />
         <button>Search</button>
 
       </div>
