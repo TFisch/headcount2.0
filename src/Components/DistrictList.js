920c4461b51679f3b53b8e1c1d13200e06402ce7
Tim Fischer <emailtimfischer@gmail.com>
2018-08-16 15:42:45 -0600

    Return compareCards in div on DistrictList

--- src/Components/DistrictList.js
+++ src/Components/DistrictList.js
@@ -3,18 +3,25 @@ import '../styling/DistrictList.css';
 import DistrictCard from './DistrictCard';
 import PropTypes from 'prop-types';
 
-const DistrictList = ({ districts, filteredDistricts }) => {
+const DistrictList = ({ districts, filteredDistricts, compareCards }) => {
   const filteredDisplay = filteredDistricts.map((district, index) => {
-
     return (<DistrictCard key={district.location} location={district.location} stats={district.stats} />)
-
-
   });
 
+  const compareTwoDistricts = compareCards.map(card => {
+    return (<DistrictCard key={district.location} location={district.location} />)
+  })
+
+
   return
-  < div className="district-list">
-    {filteredDisplay}
-  </div >;
+  <React.Fragment>
+    <div className="compare-cards">
+      {compareTwoDistricts}
+    </div>
+    <div className="district-list">
+      {filteredDisplay}
+    </div >
+  </React.Fragment>
 };
 
 DistrictList.PropTypes = {
