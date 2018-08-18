dd3440944a6557d636f4d0e3f5446d02c2ad7305
Tim Fischer <emailtimfischer@gmail.com>
2018-08-17 20:07:54 -0600

    Write test for DistrictCard snapshot

--- src/test/Component/DistrictCard.test.js
+++ src/test/Component/DistrictCard.test.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { shallow, mount } from 'enzyme';
 import DistrictCard from '../../Components/DistrictCard';
+
+describe('DistrictCard', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<DistrictCard />);
+    expect(wrapper).toMatchSnapshot()
+  });
+
+
+});
