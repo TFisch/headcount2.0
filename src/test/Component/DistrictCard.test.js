ad349741091f4c36a043c0ed05d9ed10c6fe6b7b
Tim Fischer <emailtimfischer@gmail.com>
2018-08-18 13:25:30 -0600

    Add test to set initial selected state

--- src/test/Component/DistrictCard.test.js
+++ src/test/Component/DistrictCard.test.js
@@ -3,10 +3,37 @@ import { shallow, mount } from 'enzyme';
 import DistrictCard from '../../Components/DistrictCard';
 
 describe('DistrictCard', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<DistrictCard />);
-    expect(wrapper).toMatchSnapshot()
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<DistrictCard
+      key={'key'}
+      location={'location'}
+      stats={'stats'}
+      retrieveCompare={jest.fn()}
+      comparedCards={[]}
+      removeCompareCard={jest.fn()} />);
+  })
+
+
+
+  it.skip('should match the snapshot', () => {
+    const wrapper = shallow(<DistrictCard
+      key={'key'}
+      location={'location'}
+      stats={'stats'}
+      retrieveCompare={jest.fn()}
+      comparedCards={[]}
+      removeCompareCard={jest.fn()} />);
   });
 
+  it('should update a card/s selected state when clicked', () => {
+    const mockState = {
+      selected: true
+    }
 
+    wrapper.simulate('click');
+
+    expect(wrapper.state()).toEqual(mockState);
+  })
 });
