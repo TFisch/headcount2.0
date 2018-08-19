c2789110432b19e227460de3e7e4439853d22dee
Tim Fischer <emailtimfischer@gmail.com>
2018-08-19 10:36:20 -0600

    Adjust data font size on compareCarD

--- src/Components/CompareCard.js
+++ src/Components/CompareCard.js
@@ -6,12 +6,12 @@ const CompareCard = ({ locationOne, locationTwo, foundAverages }) => {
   const averageKeys = (Object.values((foundAverages)));
 
   return (
-    <div className="compareCard">
-      <p className="locationHeading">{locationOne.location}</p>
+    <div className="compare-card">
+      <p className="location-heading">{locationOne.location}</p>
       <p className="data">{averageKeys[0]}</p>
-      <h4 className="locationHeading headingTwo">Compare</h4>
+      <h4 className="location-heading heading-two compare-head">COMPARE</h4>
       <p className="data">{averageKeys[2]}</p>
-      <p className="locationHeading headingTwo">{locationTwo.location}</p>
+      <p className="location-heading heading-two">{locationTwo.location}</p>
       <p className="data">{averageKeys[1]}</p>
     </div >
   )
