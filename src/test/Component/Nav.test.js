eb172d4f693162c1071b27c173e500722397a677
Tim Fischer <emailtimfischer@gmail.com>
2018-08-19 14:31:42 -0600

    Remove unused mo0unt from nav test

--- src/test/Component/Nav.test.js
+++ src/test/Component/Nav.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Nav from '../../Components/Nav';
 
 describe('Nav', () => {
-
   it('should match the snapshot', () => {
     let wrapper = shallow(<Nav />);
-
 
     expect(wrapper).toMatchSnapshot();
   });
