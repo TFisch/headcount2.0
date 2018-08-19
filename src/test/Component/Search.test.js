53f92d3a8377a310f93fcf90f279e815a6219ad1
Tim Fischer <emailtimfischer@gmail.com>
2018-08-19 09:53:13 -0600

    Write test to check for updateFilter call

--- src/test/Component/Search.test.js
+++ src/test/Component/Search.test.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import Search from '../../Components/Search';
+
+describe('Search', () => {
+  let wrapper;
+  let updateFilter;
+
+  beforeEach(() => {
+    updateFilter = jest.fn();
+    wrapper = shallow(<Search />);
+  })
+
+  it('should call the updateFilter method on change', () => {
+
+
+  })
+})
