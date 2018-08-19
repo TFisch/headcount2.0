641babcc89b66220f6eefb54190e8f87de16eb3c
Tim Fischer <emailtimfischer@gmail.com>
2018-08-19 13:46:05 -0600

    Add logo icon

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,10 +2,14 @@ import React from 'react';
 import "../styling/Nav.css"
 import Search from '../Components/Search'
 import PropTypes from 'prop-types';
+import '../images/grad.svg';
 
 const Nav = ({ updateFilter }) => {
   return <div className="nav">
-    <h3 className="logo">HeadCount 2.0</h3>
+    <div className="left-wrapper">
+      <img className="logo-icon" src={require('../images/grad.svg')} />
+      <h3 className="logo">HeadCount 2.0</h3>
+    </div>
     <Search updateFilter={updateFilter} />
   </div>
 }
