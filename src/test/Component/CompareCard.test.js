eb3ce846b8daa4411ad40bf1e2dbc29bfe5d77bd
Tim Fischer <emailtimfischer@gmail.com>
2018-08-19 14:33:15 -0600

    remove unused variable from comparecard test

--- src/test/Component/CompareCard.test.js
+++ src/test/Component/CompareCard.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import CompareCard from '../../Components/CompareCard';
 
 describe('CompareCard', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<CompareCard locationOne={'locationOne'} locationTwo={'locationTwo'} foundAverages={jest.fn()}
-    />);
+    wrapper = shallow(
+      <CompareCard
+        locationOne={'locationOne'}
+        locationTwo={'locationTwo'}
+        foundAverages={jest.fn()}
+      />
+    );
   });
 
   it('should match the snapshot', () => {
