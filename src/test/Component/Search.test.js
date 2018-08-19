bc54f1ff97f7ba217b326e78391d048f3d6b9362
Tim Fischer <emailtimfischer@gmail.com>
2018-08-19 12:05:51 -0600

    Add Search snapShot test

--- src/test/Component/Search.test.js
+++ src/test/Component/Search.test.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 import Search from '../../Components/Search';
 
 
 describe('Search', () => {
 
+  it('should match the snapshot', () => {
+    let wrapper = shallow(<Search />);
 
-
+    expect(wrapper).toMatchSnapshot();
+  })
 
   it('should call the updateFilter method on change', () => {
     let updateFilterMock = jest.fn();
     let wrapper = mount(<Search updateFilter={updateFilterMock} />);
+
     wrapper.find('.search-input').simulate('change');
+
     expect(wrapper.props().updateFilter).toHaveBeenCalled();
-
-
   })
 })
