dc738b5907f597adbffa8ba8ef7f3c0e040d79b1
Tim Fischer <emailtimfischer@gmail.com>
2018-08-19 14:31:14 -0600

    Remove unusued variable from DistrictList

--- src/test/Component/DistrictList.test.js
+++ src/test/Component/DistrictList.test.js
@@ -1,34 +1,43 @@
-import React from "react";
-import ReactDom from 'react-dom';
-import { shallow, mount } from 'enzyme';
-import DistrictList from "../../Components/DistrictList";
-
+import React from 'react';
+import { shallow } from 'enzyme';
+import DistrictList from '../../Components/DistrictList';
 
 describe('DistrictList', () => {
   let mockFn = jest.fn();
   let mockArray = [
     {
-      locaton: "GERMANY",
+      locaton: 'GERMANY',
       stats: { 2004: 0.29, 2005: 0.238, 2006: 0.369 }
     },
     {
-      locaton: "FLORIDA",
+      locaton: 'FLORIDA',
       stats: { 2004: 0.24, 2005: 0.278, 2006: 0.337 }
     }
   ];
 
-
-
   it('should match the snapshot', () => {
-    let wrapper = shallow(<DistrictList filteredDistricts={mockArray} comparedCards={[]} retrieveCompare={mockFn} removeCompareCard={mockFn} foundAverages={{}} />);
+    let wrapper = shallow(
+      <DistrictList
+        filteredDistricts={mockArray}
+        comparedCards={[]}
+        retrieveCompare={mockFn}
+        removeCompareCard={mockFn}
+        foundAverages={{}}
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render the comparedCards and average card when there are two in the compared array', () => {
-    let wrapper = shallow(<DistrictList filteredDistricts={mockArray} comparedCards={mockArray} retrieveCompare={mockFn} removeCompareCard={mockFn} foundAverages={{}} />);
+    let wrapper = shallow(
+      <DistrictList
+        filteredDistricts={mockArray}
+        comparedCards={mockArray}
+        retrieveCompare={mockFn}
+        removeCompareCard={mockFn}
+        foundAverages={{}}
+      />
+    );
     expect(wrapper).toMatchSnapshot();
-
   });
-
 });
-
